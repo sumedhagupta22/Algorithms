0fa203b9ffe94607baf5fd21fa373566124f7122
Sumedha Gupta <sumedha.gupta22@gmail.com>
2017-07-17 16:48:38 -0500

    Update MergeBinaryTress.java

--- MergeBinaryTress.java
+++ MergeBinaryTress.java
@@ -45,11 +45,11 @@ public class MergeBinaryTress {
 
 }
  
-//class TreeNode
-//{
-//	public int val;
-//	public TreeNode left;
-//	public TreeNode right;
-//	public TreeNode(int x) { val=x;}
-//	
-//}
+class TreeNode
+{
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int x) { val=x;}
+	
+}
