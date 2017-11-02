075b3a4260e32de2d7cff5666d5e4fb16410f619
Sumedha Gupta <sumedha.gupta22@gmail.com>
2017-11-02 16:47:17 -0500

    Update BinaryTree.java

--- Trees and Graphs/BinaryTree.java
+++ Trees and Graphs/BinaryTree.java
@@ -1,1 +1,16 @@
 
+package graphs;
+
+public class BinaryTree {
+
+	TreeNode root;
+}
+
+class TreeNode
+{
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int x) { val=x;}
+	
+}
