a641690d450a65c3aa79de3f6ce3e8e137047862
Sumedha Gupta <sumedha.gupta22@gmail.com>
2018-03-14 12:34:20 -0500

    Swap LinkedList Nodes in Pair

---
+++ Linked List/SwapNodesInPair.java
@@ -0,0 +1,49 @@
+package com.sgupta50.arrays;
+
+public class SwapNodesInPair
+{
+
+    public static void main(String args[])
+    {
+
+        ListNode head = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+
+        head.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+
+        System.out.println("before swap");
+
+        ListNode swapHead = swapPairs(head);
+        while(swapHead != null)
+        {
+            System.out.println(swapHead.val);
+            swapHead = swapHead.next;
+        }
+    }
+
+    public static ListNode swapPairs(ListNode head)
+    {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+        ListNode second = head.next;
+        ListNode third = head.next.next;
+        head.next = swapPairs(third);
+        second.next = head;
+        return second;
+    }
+}
+
+//Definition for singly-linked list.
+class ListNode
+{
+    int      val;
+    ListNode next;
+
+    ListNode(int x) { val = x; }
+}
